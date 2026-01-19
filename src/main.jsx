d3c89ab9b9debd7ff0ae353307232eb13c13aaca
hieuknguyen <nguyentrunghieu4390@gmail.com>
2026-01-19 09:46:28 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
+import { ToastProvider } from './components/Toast/Toast'
 import router from './routes'
 import './styles/main.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
+      <ToastProvider>
+        <RouterProvider router={router} />
+      </ToastProvider>
     </AuthProvider>
   </StrictMode>,
 )
+
